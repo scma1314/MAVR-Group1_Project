29c92195e501f5145d68df72ee666387947f7145
scma1314 <scma1314@h-ka.de>
2022-05-23 16:17:59 +0200

    initaial commit of marcel_dev

---
+++ Assets/Skripte/Scrips Marcel/testscripts/sandbox_testscript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Skripte.Scrips_Marcel
+{
+	public class sandbox_testscript : ScriptableObject
+	{
+		[MenuItem("Tools/MyTool/Do It in C#")]
+		static void DoIt()
+		{
+			EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
+		}
+	}
+}
